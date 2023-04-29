d1c0d5859c04ebd3aa02e37a9fd91cb7de58f961
Ig Momin Khan <mominkhan8991@gmail.com>
2023-04-29 22:19:18 +0300

    authentication system added

--- src/firebase/firebase.config.jsx
+++ src/firebase/firebase.config.jsx
@@ -6,13 +6,15 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmSaM4PVuiDzx9VhkPm4mpsBnrrpg09GA",
-  authDomain: "fir-authentication-415df.firebaseapp.com",
-  projectId: "fir-authentication-415df",
-  storageBucket: "fir-authentication-415df.appspot.com",
-  messagingSenderId: "436677803972",
-  appId: "1:436677803972:web:d42387d4c52135011bae91"
+  apiKey:import.meta.env.VITE_API_KEY,
+  authDomain:import.meta.env.VITE_AUTH_DOMAIN ,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
+
+console.log(firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
